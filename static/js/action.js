12f9c556125d8b4c119511cb30f82ca102cd5e95
jennifurhe <islandhe@gmail.com>
2016-11-17 20:41:15 -0600

    css

---
+++ static/js/action.js
@@ -0,0 +1,11 @@
+//Empty get function that will change the cover image
+function changeCoverImage(){
+    var image = document.getElementById("cover_image");
+    //image.src = string; stirng will be a url that changes the image
+}
+
+//Empty get function that will change the artists info
+function changeCoverArtist(){
+    var text = document.getElementById("song_artist");
+    // text.innerHTML='['+string+']'; string will be the artist of current song
+}
